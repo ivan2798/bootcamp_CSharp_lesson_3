7b7cb1cc2a284639db381275e6db0d02f87c713a
ivan2798 <inge1998@hotmail.com>
2022-10-17 15:38:29 -0500

    Condicional IF I

---
+++ C_Sharp_bootcamp_lesson3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+// Hello World! program
+namespace C_Sharp_bootcamp_lesson3
+{
+    class Program
+    {
+        // intellisense
+
+        static void Main(string[] args)
+        {
+            int edad = 25;
+
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+
+            if(edad >= 18)
+            {
+                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+            }
+        }
+
+       
+    }
+}
